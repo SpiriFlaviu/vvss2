6a6048fb9a39cbd8e98036e59a6c08280ce7f882
SpiriFlaviu <flaviuspiridusu@yahoo.com>
2017-05-24 09:04:07 +0300

    ok

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by ingridmayer on 29/03/2017.
+ * Created by Flaviu on 29/03/2017.
  */
 
 public class StudentTest {
